0de3d5f2f67116db487c531434097aed1d79b333
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-05-29 06:28:20 +0300

    ADD localStorge by lib(redux-presist)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { Header, Footer } from "@components/.";
-import { store } from "@store/store";
+import { store, persistor } from "@store/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const App = () => {
   return (
     <div className="container flex min-h-screen flex-col font-mono">
       <Provider store={store}>
-        <Header />
-        <main className="flex-grow">
-          <Outlet />
-        </main>
-        <Footer />
+        <PersistGate persistor={persistor} loading={null}>
+          <Header />
+          <main className="flex-grow">
+            <Outlet />
+          </main>
+          <Footer />
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import categoriesReducer from "./categories/categoriesSlice";
 import productsReducer from "./products/productsSlice";
 import cartReducer from "./cart/cartSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-  reducer: {
-    categories: categoriesReducer,
-    products: productsReducer,
-    cart: cartReducer,
-  },
+const cartPersistConfig = {
+  key: "cart",
+  storage,
+  whitelist: ["items"],
+};
+
+const rootReducer = combineReducers({
+  categories: categoriesReducer,
+  products: productsReducer,
+  cart: persistReducer(cartPersistConfig, cartReducer),
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+
+export type { RootState, AppDispatch };
+export { store, persistor };
