475ad490d75627da855a57d028cc4aa999a3369b
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-06-05 11:57:01 +0300

    FIX errors

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,6 +1,6 @@
 import { Loading, Product, GridList } from "@components/.";
 import { useAppSelector, useAppDispatch } from "@store/hooks";
-import { actGetProducts } from "@store/products/productsSlice";
+import { actGetProducts, cleanUp } from "@store/products/productsSlice";
 import { TProduct } from "@types/product";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
@@ -9,17 +9,15 @@ const Products = () => {
   const params = useParams();
   const { loading, error, records } = useAppSelector((state) => state.products);
   const items = useAppSelector((state) => state.cart.items);
-  const productsFullInfo: TProduct[] = records.map((el) => {
-    return {
+  const productsFullInfo: TProduct[] = records.map((el) => ({
       ...el,
       quantity: items.find((id) => id.id === el.id)?.quantity || 0,
-    };
-  });
+    }));
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(actGetProducts(params.id ? params.id : "all"));
     return () => {
-      dispatch(cleanUp);
+      dispatch(cleanUp());
     };
   }, [dispatch, params.id]);
   return (
--- src/store/wishlist/act/actGetProductsWhislist.ts
+++ src/store/wishlist/act/actGetProductsWhislist.ts
@@ -1,39 +1,30 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "@store/store";
 import { TProduct } from "@types/product";
 import axios from "axios";
 
-type TWishlist = { productId: string; userId: string; id: string }[];
 type TData = TProduct[];
 
 const actGetProductsWhislist = createAsyncThunk(
   "wishlist/actGetProductsWhislist",
-  async (_, { rejectWithValue }) => {
+  async (_, { rejectWithValue, getState }) => {
     let data: TData = [];
     let error = false;
     let errorMag = "";
+    const {
+      wishlist: { itemsId },
+    } = getState() as RootState;
     await axios
-      .get<TWishlist>(`/wishlist?userId=1`)
-      .then((res) => {
-        return res.data;
-      })
-      .then(async (wishlistIds) => {
-        await axios
-          .get<TProduct[]>("/products")
-          .then((res) => res.data)
-          .then((allProducts) => {
-            data = allProducts.filter((el) =>
-              wishlistIds.some(({ productId }) => productId === el.id),
-            );
-          })
-          .catch((err) => {
-            error = true;
-            errorMag = err.message;
-          });
+      .get<TProduct[]>("/products")
+      .then((res) => res.data)
+      .then((allProducts) => {
+        data = allProducts.filter((el) => itemsId.includes(el.id));
       })
       .catch((err) => {
         error = true;
         errorMag = err.message;
       });
+
     return error ? rejectWithValue(errorMag) : data;
   },
 );
