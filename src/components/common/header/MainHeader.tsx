118ad7625d3243331a277fb0830ef72f77418e79
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-06-06 22:11:37 +0300

    UPDATE header counter to reuse component

--- src/components/common/header/MainHeader.tsx
+++ src/components/common/header/MainHeader.tsx
@@ -6,41 +6,10 @@ import {
 } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { CartQuantitySeletor } from "@store/cart/selector/selector";
-import { useAppSelector } from "@store/hooks";
-import { useEffect, useRef, useState } from "react";
 import { wishlistQuantitySeletor } from "@store/wishlist/selector/selector";
+import HaederCounter from "@components/eCommerce/HaederCounter";
 
 const MainHeader = () => {
-  const cartQuantity = useAppSelector(CartQuantitySeletor);
-  const wishlistQuantity = useAppSelector(wishlistQuantitySeletor);
-  const [isAddCart, setIsAddCart] = useState(false);
-  const [isAddWishlist, setIsAddWishlist] = useState(false);
-  const firstUpdateCart = useRef(true);
-  const firstUpdateWishlist = useRef(true);
-  useEffect(() => {
-    if (firstUpdateCart.current) {
-      firstUpdateCart.current = false;
-      return;
-    }
-    setIsAddCart(true);
-    const timeout = setTimeout(() => {
-      setIsAddCart(false);
-    }, 300);
-    return () => clearTimeout(timeout);
-  }, [cartQuantity]);
-
-  useEffect(() => {
-    if (firstUpdateWishlist.current) {
-      firstUpdateWishlist.current = false;
-      return;
-    }
-    setIsAddWishlist(true);
-    const timeout = setTimeout(() => {
-      setIsAddWishlist(false);
-    }, 300);
-    return () => clearTimeout(timeout);
-  }, [wishlistQuantity]);
-
   return (
     <div className="flex items-center justify-between">
       <Link to="/" className="py-10 text-4xl font-bold uppercase">
@@ -51,36 +20,16 @@ const MainHeader = () => {
         <div>
           <Account size={32} />
         </div>
-        <Link to="/wishlist">
-          <div className="relative">
-            <Wishlist size={32} />
-            {wishlistQuantity !== 0 && (
-              <div
-                className={
-                  "absolute -bottom-2 -left-1 flex h-5 w-5 items-center justify-center rounded-full bg-red-500" +
-                  (isAddWishlist ? " animate-pumping" : "")
-                }
-              >
-                <span className="text-xs text-white">{wishlistQuantity}</span>
-              </div>
-            )}
-          </div>
-        </Link>
-        <Link to="/cart">
-          <div className="relative">
-            <ShoppingBag size={32} />
-            {cartQuantity !== 0 && (
-              <div
-                className={
-                  "absolute -bottom-2 -left-1 flex h-5 w-5 items-center justify-center rounded-full bg-red-500" +
-                  (isAddCart ? " animate-pumping" : "")
-                }
-              >
-                <span className="text-xs text-white">{cartQuantity}</span>
-              </div>
-            )}
-          </div>
-        </Link>
+        <HaederCounter
+          quantitySeletor={wishlistQuantitySeletor}
+          url="/wishlist"
+          Icon={<Wishlist size={32} />}
+        />
+        <HaederCounter
+          quantitySeletor={CartQuantitySeletor}
+          url="/cart"
+          Icon={<ShoppingBag size={32} />}
+        />
       </div>
     </div>
   );
