f15c423311445418315d800c7e9c9599f38dbfb4
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-06-07 17:33:06 +0300

    UDATE replace vite-tsconfig-pahts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tsconfigPahts from "vite-tsconfig-paths";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPahts()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/router"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@util": path.resolve(__dirname, "src/utils"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@types": path.resolve(__dirname, "src/types"),
+    },
+  },
+  plugins: [react()],
 });
