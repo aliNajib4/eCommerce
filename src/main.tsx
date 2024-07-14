d208e0f3b09b0aa7b8166d3972ce0dd942ddd899
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-07-14 19:36:37 +0300

    ADD base scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./styles/main.scss";
 import AppRouter from "@routes/AppRouter";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<AppRouter />);
