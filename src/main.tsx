9c947e4e8f64922ad1c8d8f4b9131529e944af61
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-07-16 12:08:47 +0300

    ADD strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./styles/main.scss";
 import AppRouter from "@routes/AppRouter";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<AppRouter />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>,
+);
