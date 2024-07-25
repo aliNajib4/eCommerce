934ab0268bd34cb2487def1da587561e24c2d469
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-07-25 09:15:57 +0300

    UPDATE data products

--- src/store/products/act/actGetTopSellingProducts.ts
+++ src/store/products/act/actGetTopSellingProducts.ts
@@ -2,12 +2,10 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { type TProduct } from "@types/.";
 import { fetchGetData } from "@util/.";
 
-type TData = TProduct[];
-
 const actGetTopSellingProducts = createAsyncThunk(
   "products/actGetTopSellingProducts",
   async (_, { rejectWithValue, signal }) => {
-    const { error, errorMag, data } = await fetchGetData<TData>(
+    const { error, errorMag, data } = await fetchGetData>(
       `/top-selling`,
       signal,
     );
--- src/types/product.ts
+++ src/types/product.ts
@@ -2,16 +2,17 @@ type TProduct = {
   id: string;
   title: string;
   price: number;
-  cat_prefix?: string;
+  cat_prefix: string;
   main_img: string;
   "Sub-imgs": string[];
   max?: number;
   quantity?: number; //???
   rating: number;
   discount?: number;
-  color: string;
-  size: string;
+  color: string[];
+  size: string[];
   type: string;
+  description: string;
 };
 
 export default TProduct;
