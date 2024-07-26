f6687163326072491495c205cb404ed4782d4836
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-07-26 16:27:32 +0300

    FIX url product

--- src/components/eCommerce/Product.tsx
+++ src/components/eCommerce/Product.tsx
@@ -4,13 +4,13 @@ import { Rating, Price } from "@components/.";
 import { Link } from "react-router-dom";
 
 const Product = memo(
-  ({ main_img, title, price, rating, discount }: TProduct) => {
+  ({ main_img, title, price, rating, discount, id }: TProduct) => {
     return (
       <div className="product">
         <div className="image">
           <img src={main_img} alt={title} />
         </div>
-        <Link to="." className="title" title={title}>
+        <Link to={"/product/" + id} className="title" title={title}>
           {title}
         </Link>
         <Rating rating={rating} width={18} />
